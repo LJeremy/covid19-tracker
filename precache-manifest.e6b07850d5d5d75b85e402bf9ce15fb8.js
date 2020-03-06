ec2ee47ba828332c0dabdaad8b37dddea6b6310d
Lars Van Houwelingen <lars@keytoe.nl>
2020-03-06 18:41:49 +0100

    Updates

---
+++ precache-manifest.e6b07850d5d5d75b85e402bf9ce15fb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1857a8e6049a00f20b4eb00624818b",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "a332725084b7bf029902",
+    "url": "/covid19-tracker/static/js/2.9c42db63.chunk.js"
+  },
+  {
+    "revision": "87b92515399d3de65f60490e1646235f",
+    "url": "/covid19-tracker/static/js/2.9c42db63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982b380793faec5e2c51",
+    "url": "/covid19-tracker/static/js/main.4364aa3a.chunk.js"
+  },
+  {
+    "revision": "f2b80709a851a9bd0ed9",
+    "url": "/covid19-tracker/static/js/runtime-main.256ebdba.js"
+  }
+]);
